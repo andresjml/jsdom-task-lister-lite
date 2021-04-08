ddee6f2ec0c38e1f3670dd5f4097b6eee19926c6
Andres <andresjml@gmail.com>
2021-04-07 21:57:20 -0400

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+let taskFormSubmit = document.getElementById('submit');
+let ul = document.getElementById('tasks');
+    taskFormSubmit.addEventListener('click', function(e){
+    e.preventDefault();    
+    let li = document.createElement('li');
+    li.innerHTML =document.getElementById('new-task-description').value;  
+    let button = document.createElement('button');  
+    button.innerHTML='X'
+    button.id = document.getElementById('new-task-description').value;
+    li.appendChild(button)
+    ul.appendChild(li);  
+    button.addEventListener('click', function(){  
+      ul.removeChild(li);
+    });
+  });
 });
